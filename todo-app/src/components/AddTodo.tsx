3ee93cf207a3fad718d31b4e8017162bcdb60935
Nivisan Jeyananthan <33133411+Nivisan-Jeyananthan@users.noreply.github.com>
2026-04-16 22:19:57 +0000

    fix: crypto uuid to randomvalues
    crpyto.uuid not supported in unsecure environments

--- todo-app/src/components/AddTodo.tsx
+++ todo-app/src/components/AddTodo.tsx
@@ -1,4 +1,5 @@
 import { useState, ChangeEvent, KeyboardEvent } from "react";
+import { getRandomUUID } from "../utils/CryptoUtils";
 import type { JSX } from "react";
 import {
   TodoItemType,
@@ -33,7 +34,7 @@ export default function TodoInput(props: ITodoInputProps): JSX.Element {
   const handleClickCreate = () => {
     if (todoText && dispatch) {
       const temp: TodoItemType = {
-        Id: crypto.randomUUID(),
+        Id: getRandomUUID(),
         Text: todoText,
         Status: TodoStatus.New,
         ParentId: props.parentId,
